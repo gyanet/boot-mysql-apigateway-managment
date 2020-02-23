26c2b59cf199b194abcf39d55f91c9984e688331
GYPR <giselle.designe@gmail.com>
2020-02-23 09:47:27 +0100

    Delete validation in requests params

--- src/main/java/com/apigateway/managment/taskapigateway/error/ErrorResponse.java
+++ src/main/java/com/apigateway/managment/taskapigateway/error/ErrorResponse.java
@@ -3,7 +3,6 @@ package com.apigateway.managment.taskapigateway.error;
 import com.apigateway.managment.taskapigateway.utils.ResponseType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,9 +16,9 @@ public class ErrorResponse {
     private ResponseType type;
     private String message;
     private String debugMessage;
+    private List<SubError> details;
     private String path;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private Date date = new Date();
-    private List<SubError> subErrors;
 
 }
