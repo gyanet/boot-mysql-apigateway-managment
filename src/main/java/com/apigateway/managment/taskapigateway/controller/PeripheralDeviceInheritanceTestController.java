cfc673f0cc48e7d80c138836abebcb58cfc8eb8d
giselleyanet <giselle.designe@gmail.com>
2020-03-27 10:48:21 -0400

    Configuring Locale PeripheralDeviceInheritanceTestController

--- src/main/java/com/apigateway/managment/taskapigateway/controller/PeripheralDeviceInheritanceTestController.java
+++ src/main/java/com/apigateway/managment/taskapigateway/controller/PeripheralDeviceInheritanceTestController.java
@@ -5,10 +5,13 @@ import com.apigateway.managment.taskapigateway.dto.PeripheralDeviceOutDTO;
 import com.apigateway.managment.taskapigateway.service.IPeripheralDeviceInheritanceTestService;
 import com.apigateway.managment.taskapigateway.service.implementation.PeripheralDeviceInheritanceTestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Description;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.Locale;
 
 @RestController
 @Description(value = "")
@@ -16,11 +19,20 @@ import javax.websocket.server.PathParam;
 public class PeripheralDeviceInheritanceTestController implements IPeripheralDeviceInheritanceTestService {
 
     @Autowired
+    MessageSource messageSource;
+
+    @Autowired
     PeripheralDeviceInheritanceTestService peripheralDeviceInheritanceTestService;
 
     @GetMapping(path = "/in/{id}", produces = "application/json")
     public PeripheralDeviceInDTO findInById(@PathVariable("id") Long idPeripheral) throws Exception {
         return peripheralDeviceInheritanceTestService.findInById(idPeripheral);
+    }
+
+    @GetMapping(path = "/locale/{lang}", produces = "application/json")
+    public String getLocaleMessage(@PathVariable("lang") String lang) throws Exception {
+        LocaleContextHolder.setLocale(Locale.forLanguageTag(lang));
+        return messageSource.getMessage("label.welcome",null, LocaleContextHolder.getLocale());
     }
 
     @PostMapping(path = "", produces = "application/json")
